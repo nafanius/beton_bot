c9f62df731d18af1c368cb12369025e9ae3ebaa6
ilin <fizruk.ilin@gmail.com>
2025-01-13 15:10:10 +0100

    dev(setting) add setting file

---
+++ setting.py
@@ -0,0 +1,12 @@
+
+
+
+class Settings:
+    data_base = 'sqlite:///data_bot_bethon.db'
+    # id_group = "-4533287060"
+    ADMIN_ID = ["-4533287060", ] # куда шлём
+    message_without_bot = "Чёто ты меня притомил, давай ка помолчим kurwa"
+    client = Wit('HZZJUIX7N6O7LJ2XNNSPN2ZTFGLWQCF6')
+
+    def __init__(self):
+        pass
