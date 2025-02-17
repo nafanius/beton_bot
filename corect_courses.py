74836a9065b932578f63af24593226f5c4ea087c
ilin <fizruk.ilin@gmail.com>
2025-02-17 22:48:02 +0100

    dev(corect_courses.py) added cjrected course by message

--- corect_courses.py
+++ corect_courses.py
@@ -3,6 +3,26 @@ import time
 import data_sql_list
 import pandas as pd
 import threading
+import logging
+
+
+# region logging
+
+logging.basicConfig(level=logging.DEBUG,
+                    format="%(asctime)s - %(levelname)s - %(message)s")
+lg = logging.debug
+cr = logging.critical
+inf = logging.info
+exp = logging.exception
+# logging.disable(logging.DEBUG)
+# logging.disable(logging.INFO)
+# logging.disable(logging.CRITICAL)
+# logging_end
+# endregion
+
+
+
+
 
 db_lock = threading.Lock()
 
@@ -10,9 +30,12 @@ db_lock = threading.Lock()
 def save_corect_course(number, name_user, new_time=time.time()):
     query = f'SELECT * FROM actual WHERE "index" = {number}'
 
+    try:
+        with db_lock:
+            df_restored_query = pd.read_sql_query(query, con=data_sql_list.engine)
+    except Exception as error:
+        inf(f"ошибка запроса из базы actual {error}")
 
-    with db_lock:
-        df_restored_query = pd.read_sql_query(query, con=data_sql_list.engine)
 
     df_restored_query[['new_time', 'user']] = new_time, name_user
 
