fea32ea2dc9f96eec064ce7ea80679e9a961930e
ilin <fizruk.ilin@gmail.com>
2025-05-24 11:43:02 +0200

    dev(jupyter/Untitled.ipynb) correcting comments main.py

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import json
-import logging
 import time
 import os
 from datetime import datetime
@@ -27,12 +26,14 @@ from src.setting import Settings, inf, lg, timer
 from src.get_request import answer_to_request
 
 
-client = Wit(auth_data.cod_wit)
+client_Wit = Wit(auth_data.cod_wit)
 name_bud = ""
 db_lock = threading.Lock()
 
 
 def restart_service():
+    """Restart the service using systemctl
+    """    
     try:
         subprocess.run(['sudo','systemctl', 'restart', 'my_bot_bet.service'])
         lg("Service restarted successfully.")
@@ -41,13 +42,12 @@ def restart_service():
 
 
 # region SAVE AND LOAD JSON
-# Функция для записи словаря в файл
+# todo region save and load json, rebuild it with db
 def save_dict_to_file(dictionary, filename):
     with open(filename, 'w', encoding='utf-8') as f:
         json.dump(dictionary, f, ensure_ascii=False, indent=4)
 
 
-# Функция для загрузки словаря из файла
 def load_dict_from_file(filename):
     if os.path.isfile(filename):
         with open(filename, 'r', encoding='utf-8') as f:
@@ -63,10 +63,14 @@ def load_dict_from_file(filename):
 
 
 def telegram_bot(token):
-    """основной цикл следящий за состоянием"""
+    """main function for telegram bot
+
+    Args:
+        token (str): token for telegram bot
+    """    
     bot = telebot.TeleBot(token)
 
-    # Переменные для хранения состояний пользователя
+    # todo make it as a database
     dict_contacts = {"Palcasty": "+48570315464",
                      "Ighor": "+48572989696",
                      "Maks": "+48536519415",
@@ -80,15 +84,17 @@ def telegram_bot(token):
                      "HOLCIM węzeł 1": "+48519537060"}
 
     def send_scheduled_message():
-        """функция отсыла сообщений по утрам"""
+        """function for sending scheduled messages
+        """        
+        
         try:
             while True:
                 now = datetime.now()
                 if now.weekday() > 5:
-                    time.sleep(10800)  # Подождите 3 часа  перед следующей проверкой, если это выходной день
+                    time.sleep(10800)  # check every 3 hours on weekends
                     continue
 
-                # Проверьте если текущее время совпадает с запланированным (например, 9:00)
+                # check if it's time to send the morning message
                 if now.hour == 6 and now.minute == 30:
                     weather_3day = weather.weather_3day()
                     bot.send_message(Settings.ID_GROUPS[0], f"<b>Dzień dobry, panowie!</b>\n\n"
@@ -99,10 +105,11 @@ def telegram_bot(token):
                                             f"Temperatura odczuwalna <b><u>{weather_3day[0]['temp']}</u></b>\n"
                                             f"zachmurzenie <b><u>{weather_3day[0]['облачность']}</u></b>\n"
                                             f"wiatr <b><u>{weather_3day[0]['ветер']}</u></b>\n", parse_mode='HTML')
-                    time.sleep(90)  # Пауза, чтобы избежать многократной отправки в течение той же минуты
+                    time.sleep(90)  # check every 90 seconds to avoid multiple sends in the same minute
 
+                # check if it's time to send the evening message
+                # send infojrmation about changes in chromonogram 
                 if now.minute in [7, 27, 47] :
-                    inf("*************я сработал ***************************************************************")
                     text_list_beton = lista_in_text_beton()
                     text_lista = src.get_lista.combination_of_some_days_list()
 
@@ -116,19 +123,23 @@ def telegram_bot(token):
                             bot.send_message(id, text_lista, parse_mode='HTML')
                             time.sleep(2)
 
-                    time.sleep(90)  # Пауза, чтобы избежать многократной отправки в течение той же минуты
-                time.sleep(20)  # Проверка каждые 20 секунд
-        except Exception as err:
+                    time.sleep(90)  # puse for 90 seconds to avoid multiple sends in the same minute
+                time.sleep(20)  # check every 20 seconds
+        except Exception as err:  # if error occurs, log it and restart the service
             inf(err)
             restart_service()
                 
 
-    # Запускаем поток для выполнения запланированного задания
+    # lounch thread for scheduled messages
     threading.Thread(target=send_scheduled_message).start()
 
     @bot.message_handler(content_types=['new_chat_members'])
     def welcome_new_member(message):
-        """запуск при входе нового пользователя"""
+        """triggered when a new member joins the chat
+
+        Args:
+            message (object): passed from wrapper telebot, contains information about the new member
+        """        
         for new_member in message.new_chat_members:
             bot.send_message(message.chat.id,
                              f"Witamy, <b>{new_member.first_name}!</b>\n"
@@ -144,11 +155,12 @@ def telegram_bot(token):
     @bot.callback_query_handler(func=lambda call: True)
     def handle_callback(call):
         answer_text = []
-        """"оброботка сробатывания кнопок"""
-        if call.data == "button1":  # расписание
+        """processing tap on button in inline keyboard
+        """        
+        if call.data == "button1":  # shedule
             answer_text.append(src.get_lista.combination_of_some_days_list(True))
 
-        elif call.data == "button2":  # погода
+        elif call.data == "button2":  # weather
             try:
                 weather_day = weather.weather_now()
                 weather_3day = weather.weather_3day()
@@ -174,7 +186,8 @@ def telegram_bot(token):
             except Exception as err:
                 inf(err)
                 return
-        elif call.data == "button3":  # будовы
+        
+        elif call.data == "button3":  # construction sites
             answer = []
             dic_bud = load_dict_from_file('dic_bud.json')
             for key in dic_bud.keys():
@@ -182,12 +195,14 @@ def telegram_bot(token):
                               f'*{key}*</a>')
 
             answer_text.append("Budowy:\npo kliknięciu otworzy się geolokalizacja\n" + "\n".join(answer))
-        elif call.data == "button4":
+        
+        elif call.data == "button4": # contacts
             list_of_phone = []
             for key in dict_contacts.keys():
                 list_of_phone.append(f'{key} <a href="tel:{dict_contacts[key]}">{dict_contacts[key]}</a>')
             answer_text.append('\n'.join(list_of_phone))
-        elif call.data == "button5":
+        
+        elif call.data == "button5": # where to sell concrete
             answer_text.append((f'<a href="https://www.google.pl/maps/place/MD+Beton+Marek+D%C4%'
                            f'85browski/@52.1922286,20.7767505,17z/data=!3m1!4b1!4m6!3m5!1s0x4719'
                            f'352a34873e2b:0xc1fcd68e6bb8d915!8m2!3d52.1922253!4d20.7793254!16s%2'
@@ -201,7 +216,8 @@ def telegram_bot(token):
                            f'=ttu&g_ep=EgoyMDI0MTAyOS4wIKXMDSoASAFQAw%3D%3D">'
                            f'*Pawel:*</a>\n'
                            f'ТЕЛЕФОН: <a href="tel:+48505966026">+48505966026</a>\n\n'))
-        elif call.data == "button6":
+        
+        elif call.data == "button6": # loading schedule
             answer_text = lista_in_text_beton(False)
 
         try:
@@ -222,9 +238,13 @@ def telegram_bot(token):
 
     @bot.message_handler(commands=['start'])
     def start_message(message):
-        """сробатывание на команду слэш с"""
+        """answering on command /start
+
+        Args:
+            message (object): passed from wrapper telebot, contains information about the message
+        """        
         print(message.chat.id)
-        markup = types.InlineKeyboardMarkup()  # Создаем разметку с кнопками
+        markup = types.InlineKeyboardMarkup()  # markup for inline keyboard
         btn1 = types.InlineKeyboardButton("rozkład", callback_data="button1")
         btn2 = types.InlineKeyboardButton("pogodę", callback_data="button2")
         btn3 = types.InlineKeyboardButton("budowy", callback_data="button3")
@@ -240,7 +260,11 @@ def telegram_bot(token):
     # help
     @bot.message_handler(commands=['h'])
     def help_message(message):
-        """сробатывание на команду слэш аш"""""
+        """answering on command /h
+
+        Args:
+            message (object): passed from wrapper telebot, contains information about the message
+        """        
         bot.send_message(message.chat.id, f"{message.from_user.first_name}\n"
                                           f"Jestem botem, który pomaga dostarczyć wszystkie niezbędne informacje dla początkujących i"
                                           f" zaawansowanych operatorów betonomeszarek\n"
@@ -254,6 +278,11 @@ def telegram_bot(token):
 
     @bot.message_handler(commands=["lista"])
     def send_lista(message):
+        """answering on command /lista
+
+        Args:
+            message (object): passed from wrapper telebot, contains information about the message
+        """        
         bot.send_message(message.chat.id, "Oto ci, kurwa, rozkład: https://bit.ly/holcim_lista")
 
     @bot.message_handler(commands=["co"])
@@ -264,13 +293,22 @@ def telegram_bot(token):
     # region ADD BUDOWA
     @bot.message_handler(commands=['add'])
     def add_budowa(message):
-        """записываем адрес и локализацию будовы"""
+        """answering on command /add
+
+        Args:
+            message (object): passed from wrapper telebot, contains information about the message
+        """        
         msg = bot.send_message(message.chat.id, "Wprowadź nazwę", reply_markup=types.ForceReply())
         bot.register_next_step_handler(msg, ask_name_budowy)
 
-    def ask_geolocation(message):  # Ответ корректен, продолжаем
+    def ask_geolocation(message):
+        """asking for geolocation
+
+        Args:
+            message (object): passed from wrapper telebot, contains information about the message
+        """        
         if message.content_type == 'location':
-            # Ответ корректен, продолжаем
+            # answer correct, continue
             global name_bud
             lat = message.location.latitude
             lon = message.location.longitude
@@ -280,19 +318,24 @@ def telegram_bot(token):
                 save_dict_to_file(dic_bud, "dic_bud.json")
             bot.send_message(message.chat.id, "Przyjęto!", reply_markup=None)
         else:
-            # Ответ некорректен, просим ввести снова
+            # answer incorrect, ask again
             msg = bot.send_message(message.chat.id, "Wyślij lokalizację")
             bot.register_next_step_handler(msg, ask_geolocation)
 
-    def ask_name_budowy(message):  # Ответ корректен, продолжаем
+    def ask_name_budowy(message):
+        """asking for name of budowa
+
+        Args:
+            message (object): passed from wrapper telebot, contains information about the message
+        """        
         if message.content_type == 'text':
             global name_bud
-            # Ответ корректен, продолжаем
+            # answer correct, continue
             name_bud = message.text
             bot.send_message(message.chat.id, "Podaj swoją lokalizacjęУ", reply_markup=types.ForceReply())
             bot.register_next_step_handler(message, ask_geolocation)
         else:
-            # Ответ некорректен, просим ввести снова
+            # answer incorrect, ask again
             msg = bot.send_message(message.chat.id, "Wprowadź tekst - Nazwa budowy")
             bot.register_next_step_handler(msg, ask_name_budowy)
 
@@ -301,15 +344,20 @@ def telegram_bot(token):
     # text
     @bot.message_handler(content_types=['text'])
     def handle_text(message):
+        """cathe all text messages and looking first worlds Palec, ?, Chuj, хуй, and if it looks for then send answer
+
+        Args:
+            message (object): passed from wrapper telebot, contains information about the message
+        """        
         text_message = message.text
-        # Игнорируем сообщения от пользователей, которые не находятся в состоянии ожидания ответа
+        # added message from user to conversation history for chatgpt bot
         if message.content_type == 'text':
             conversation_history = load_dict_from_file('conversation_history.json')
             if len(conversation_history) > 150:
                 conversation_history = conversation_history[-75:]
             conversation_history.append({"role": "user", "content": f"{message.from_user.first_name}: {message.text}"})
 
-            bot_name = text_message.split()[0].lower()[:5]
+            bot_name = text_message.split()[0].lower()[:3]
 
             request_corect_corse = text_message.lower()
             pattern_huy = Settings.pattern_huy
@@ -327,7 +375,7 @@ def telegram_bot(token):
 
                 split_text = text_message.split()
                 text_message = ' '.join(split_text[1:])
-                # пробуем получить ответ от чат бота
+                # try to get answer from chat bot
                 try:
                     bot.reply_to(message, ask_chatgpt(text_message))
                 except:
@@ -362,11 +410,16 @@ def telegram_bot(token):
                     save_dict_to_file(conversation_history, 'conversation_history.json')
 
     def palec_voice(message):
-        """reply voice palcasty"""
+        """cathe all voice messages and looking first worlds Pal, if it looks for then send answer anather voice
+        message saved in conversation history
+
+        Args:
+            message (_type_): _description_
+        """        
         try:
-            # Получаем информацию о голосовом сообщении
+            # getting voice message file
             file_info = bot.get_file(message.voice.file_id)
-            # Скачиваем файл
+            # downloading the file
             downloaded_file = bot.download_file(file_info.file_path) # type: ignore
             ogg_audio = io.BytesIO(downloaded_file)
             audio = AudioSegment.from_file(ogg_audio, format="ogg")
@@ -375,7 +428,8 @@ def telegram_bot(token):
             wav_audio_io.seek(0)
             wav_binary_data = wav_audio_io.read()
 
-            resp = client.speech(wav_binary_data, {'Content-Type': 'audio/wav'})
+            # converting voice message to text using Wit.ai
+            resp = client_Wit.speech(wav_binary_data, {'Content-Type': 'audio/wav'})
 
             text_message = resp['text']
         except Exception as err:
@@ -383,14 +437,12 @@ def telegram_bot(token):
             return
         pass
 
-        # Игнорируем сообщения от пользователей, которые не находятся в состоянии ожидания ответа
         conversation_history = load_dict_from_file('conversation_history.json')
         if len(conversation_history) > 300:
             conversation_history = conversation_history[-150:]
 
-        # conversation_history.append({"role": "user", "content": f"{message.from_user.first_name}: {text_message}"})
         try:
-            bot_name = text_message.split()[0].lower()[:5]
+            bot_name = text_message.split()[0].lower()[:3]
         except IndexError:
             bot_name = ""
 
@@ -403,16 +455,16 @@ def telegram_bot(token):
 
             split_text = text_message.split()
             text_message = ' '.join(split_text[1:])
-            # пробуем получить ответ от чат бота
-            inf(text_message)
+            # try to get answer from chat bot
             try:
+                # get answer from chatgpt
                 tts = gTTS(ask_chatgpt(text_message), lang='ru')
-                # Создание объекта BytesIO
+                #  save answer to mp3 file
                 mp3_fp = io.BytesIO()
                 tts.write_to_fp(mp3_fp)
                 mp3_fp.seek(0)
 
-                # Конвертация MP3 в OGG
+                # convert mp3 to ogg format
                 audio = AudioSegment.from_file(mp3_fp, format="mp3")
                 ogg_fp = io.BytesIO()
                 audio.export(ogg_fp, format="ogg", codec="libopus")
@@ -429,7 +481,11 @@ def telegram_bot(token):
 
     @bot.message_handler(content_types=['voice'])
     def handle_voice(message):
-        """cathe all voices and looking first world Palec, if it looks for then send answer"""
+        """cathe all voice messages and looking first worlds Palec, if it looks for then send voice answer 
+
+        Args:
+            message (object): passed from wrapper telebot, contains information about the message
+        """        
 
         threading.Thread(target=palec_voice, args=(message,)).start()
 
--- src/setting.py
+++ src/setting.py
@@ -9,9 +9,9 @@ import time
 class Settings:
     data_base_lista = 'sqlite:////home/user/.database_lista/web_lista.db'
     data_base_bot = 'sqlite:////home/user/.database_bot/bot_database.db'
-    ID_GROUPS = ["-4533287060", "-4768722432"] # куда шлём
-    ID_SEND_BOT = ["-4533287060", "-4768722432", "1276025555"] #куда отвечает бот голосом и текстом
-    message_without_bot = "Чёто ты меня притомил, давай ка помолчим kurwa"
+    ID_GROUPS = ["-4533287060", "-4768722432"] # groups id where bot sends sheduled messages
+    ID_SEND_BOT = ["-4533287060", "-4768722432", "1276025555"] # there bot send message if he is asked
+    message_without_bot = "Coś mnie zmęczyłeś, pomilczmy trochę, kurwa" # answer bot when he is tired
     time_of_compare = 4
     start_time_co = 15 # min before request CO
     finish_time_co = 15 # min after request CO
